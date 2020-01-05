e8af9d86bdbe0eb434b8b8d6027a80b551c3a8f4
17774813651 <3438806287@qq.com>
2020-01-05 20:40:18 +0800

    百元白鸡
    百元白鸡

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z, i = 0;
+            for(int x=0;x<20;x++)
+            {
+                for(int y=0;y<33;y++)
+                {
+                    z = 100 - x - y;
+                    if(5*x+3*y+z/3==100&&z%3==0)
+                    {
+                        Console.WriteLine("{0}、公鸡：{1}" + "\t" + "母鸡：{2}" + "\t" + "小鸡：{3}", ++i, x, y, z);
+                    }
+                }
+                Console.ReadLine();
+               
+            }
+        }
+       
+    }
+
+}
